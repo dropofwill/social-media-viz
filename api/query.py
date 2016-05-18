312e72cd2e4e56bc39063213799aa8191a5cbe67
Will <whp3652@rit.edu>
2016-05-18 17:48:28 -0400

    Support LIWC and/or EmoLex criteria in the same query

--- api/query.py
+++ api/query.py
@@ -1,5 +1,14 @@
+'''
+By convention in Cypher queries:
+    t: is the matching Tweet node
+    e: is the matching EmoLex node
+    l: is the matching Liwc node
+'''
+from typing import List, Set, Union, Tuple
 from itertools import chain, combinations
 from py2neo import Graph, authenticate
+
+Num = Union[int, float, complex]
 
 emolex_keys = { 'anger', 'anticipation', 'disgust', 'fear', 'joy', 'negative',
         'positive', 'sadness', 'surprise', 'trust' }
@@ -8,80 +17,96 @@ top_level_liwc_keys = { 'affect', 'social', 'cogmech', 'percept', 'bio',
         'relativ' }
 
 '''
-EmoLex Queries
+Generate cypher queries for use later, via either pythonic interface:
+    graph.evaluate(query)
+
+or over the REST API
+
+POST: localhost:7474/db/data/transaction/commit
+Content-Type: "application/json"
+Authorization: bmVvNGo6bmVvNGpfcGFzc3dvcmQ=
+    {
+        "statements": [
+            "statement": query
+        ]
+    }
 '''
-def count_emolex_by_thresh(graph, category, thresh=0):
-    query='''
-    MATCH (t:Tweet)-[:HAS_EMOLEX]->(n:EmoLex)
-    WHERE n.{0} > {1}
+
+def count_criteria_by_thresh_query(
+        criteria: str,
+        category: str,
+        thresh: Num = 0) -> str:
+
+    node_name = _parse_single_criteria(criteria)
+    return '''
+    MATCH (l:Liwc)-[:HAS_LIWC]-(t:Tweet)-[:HAS_EMOLEX]-(e:EmoLex)
+    WHERE {0}.{1} > {2}
     RETURN COUNT(t)
-    '''.format(category, thresh)
+    '''.format(node_name, category, thresh)
 
-    return graph.evaluate(query)
+'''
 
-def general_emolex_counter(graph, where_clause):
-    query='''
-    MATCH (t:Tweet)-[:HAS_EMOLEX]->(n:EmoLex)
+'''
+def count_clause_query(where_clause: str) -> str:
+    return '''
+    MATCH (l:Liwc)-[:HAS_LIWC]-(t:Tweet)-[:HAS_EMOLEX]-(e:EmoLex)
     WHERE {0}
     RETURN COUNT(t)
     '''.format(where_clause)
 
-    print(query)
-    return graph.evaluate(query)
-
-def multiple_emolex_counter(graph, where_clauses):
-    return [ (k, general_emolex_counter(graph, c)) for k, c in where_clauses ]
+'''
 
 '''
-LIWC Queries
-'''
-def count_liwc_by_thresh(graph, category, thresh=0):
-    query='''
-    MATCH (t:Tweet)-[:HAS_LIWC]->(n:Liwc)
-    WHERE n.{0} > {1}
-    RETURN COUNT(t)
-    '''.format(category, thresh)
-
-    return graph.evaluate(query)
-
-def general_liwc_counter(graph, where_clause):
-    query='''
-    MATCH (t:Tweet)-[:HAS_LIWC]->(n:Liwc)
-    WHERE {0}
-    RETURN COUNT(t)
-    '''.format(where_clause)
-
-    print(query)
-    return graph.evaluate(query)
-
-def multiple_liwc_counter(graph, where_clauses):
-    return [ (k, general_liwc_counter(graph, c)) for k, c in where_clauses ]
+def count_multiple_clauses_query(where_clauses: List[str]) -> List[str]:
+    return [ (k, count_clause_query(c)) for k, c in where_clauses ]
 
 '''
 General helpers
 '''
 
-'''
-List[Tuple(Tuple(str), int)] -> List[Tuple(Tuple(str), int)]
-'''
-def top_k_counts(result, k=20):
-    return sorted(result, key=lambda x: -x[1])[0:k]
+def build_intersect_combinations(
+        keys: List[Tuple[str, str]],
+        thresh: Num = 0) -> List[Tuple[Set[str], str]]:
 
-def build_intersect_combinations(keys, thresh=0):
-    combos = combinations_from(keys, len(keys))
+    categories = set(_category_to_query_ref(k, l) for k, l in keys)
+    combos = _combinations_from(categories, len(categories))
     return [ (pos_set,
-        build_intersect_clause_by_thresh(pos_set, keys - pos_set, thresh))
+        build_intersect_clause_by_thresh(pos_set, categories - pos_set, thresh))
         for pos_set in combos ]
 
 '''
 returns: n.pos_cat1 > thresh AND n.neg_cat <= thresh
 '''
-def build_intersect_clause_by_thresh(pos_cat, neg_cat, thresh=0):
+def build_intersect_clause_by_thresh(
+        pos_cat: Set[str],
+        neg_cat: Set[str],
+        thresh: Num = 0) -> str:
+
     return ' AND '.join(chain(
-        [ 'n.{0} > {1}'.format(cat, thresh) for cat in pos_cat ],
-        [ 'n.{0} <= {1}'.format(cat, thresh) for cat in neg_cat ]
+        [ '{0} > {1}'.format(cat, thresh) for cat in pos_cat ],
+        [ '{0} <= {1}'.format(cat, thresh) for cat in neg_cat ]
     ))
 
-def combinations_from(cat, n):
-    return [ set(x) for x in chain.from_iterable(combinations(cat, i)
-        for i in range(1, n+1)) ]
+def _combinations_from(
+        categories: List[str],
+        extent: int) -> List[Set[str]]:
+
+    return [ set(x) for x in chain.from_iterable(combinations(categories, i)
+        for i in range(1, extent+1)) ]
+
+def _parse_single_criteria(lexicon_ref: str) -> str:
+    if lexicon_ref.lower() == 'emolex':
+        return 'e'
+    elif lexicon_ref.lower() == 'liwc':
+        return 'l'
+    else:
+        raise NotImplementedError('Only support liwc and emolex lexicon refs')
+
+'''
+Given a category and it's lexicon, return a property for a cypher querying
+e.g.
+_category_to_query_ref('bio', 'liwc') -> 'l.bio'
+'''
+def _category_to_query_ref(category: str, lexicon_ref: str) -> str:
+    print(category, lexicon_ref)
+    return '{0}.{1}'.format(_parse_single_criteria(lexicon_ref), category)
